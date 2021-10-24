093e798138d2d93777cc7c2c6ddb11e933eb3f48
Andre Silveira <alasandre@gmail.com>
2021-10-24 10:48:50 -0300

    About view

--- src/application/Main.java
+++ src/application/Main.java
@@ -8,12 +8,15 @@ import javafx.stage.Stage;
 
 
 public class Main extends Application {
+	
+	private static Scene mainScene;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
 			ScrollPane scrollPane = loader.load();
-			Scene mainScene = new Scene(scrollPane);
+			mainScene = new Scene(scrollPane);
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);
 			primaryStage.setScene(mainScene);
@@ -22,6 +25,10 @@ public class Main extends Application {
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public static Scene getMainScene() {
+		return mainScene;
 	}
 	
 	public static void main(String[] args) {
